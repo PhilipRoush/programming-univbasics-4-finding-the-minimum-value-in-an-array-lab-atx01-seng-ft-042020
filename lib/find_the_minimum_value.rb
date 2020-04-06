9e4d3f4f4b2cc9870b3701e54dd29e6da396be31
Philip Roush <roush.philip@gmail.com>
2020-04-06 12:21:29 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,8 +1,8 @@
 def find_min_value(scale)
   count = 0
-  min_value = 5
+  min_value = scale.max
   while count < scale.length do
-    if min_value > scale[count]
+    if min_value < scale[count]
       min_value = scale[count]
     end
     count += 1
